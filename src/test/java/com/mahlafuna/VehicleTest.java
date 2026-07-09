5dd2618c57765cb73753328ed29a4516262457b0
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-07-09 20:43:07 +0200

    feat(VehicleTest.java): tests for the vehicle

--- src/test/java/com/mahlafuna/VehicleTest.java
+++ src/test/java/com/mahlafuna/VehicleTest.java
@@ -7,6 +7,9 @@ import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Tests for Vehicle and all three Vehicle subclasses.
+ */
 public class VehicleTest {
 
     private Sedan  sedan;
