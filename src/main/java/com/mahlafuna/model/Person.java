d10da4ee80477829173a49aaf20bb6efaed843e0
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-07-09 21:03:04 +0200

    feat(Person.java): person class

---
+++ src/main/java/com/mahlafuna/model/Person.java
@@ -0,0 +1,8 @@
+package com.mahlafuna.model;
+
+public abstract class Person {
+    private final String firstName;
+    private final String lastName;
+    private String email;
+    private String phoneNumber;
+}
