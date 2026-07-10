2ffd94435643c4002572c5378c0af55211e7ea00
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-07-10 22:19:20 +0200

    feat(Person.java): person class implemented

--- src/main/java/com/mahlafuna/model/Person.java
+++ src/main/java/com/mahlafuna/model/Person.java
@@ -5,4 +5,47 @@ public abstract class Person {
     private final String lastName;
     private String email;
     private String phoneNumber;
+
+    public Person(String email, String phoneNumber, String lastName, String firstName) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String phoneNumber() {
+        return phoneNumber;
+    }
+
+    public void updatePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    public abstract String role();
 }
