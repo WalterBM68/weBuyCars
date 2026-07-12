9eb15895ccb01be59f3f1dabc652bed6e8193966
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-07-12 21:38:08 +0200

    feat(WalkInCustomer.java): WalkInCustomer class implemented

--- src/main/java/com/mahlafuna/model/WalkInCustomer.java
+++ src/main/java/com/mahlafuna/model/WalkInCustomer.java
@@ -1,12 +1,35 @@
 package com.mahlafuna.model;
 
-public class WalkInCustomer extends Person {
-    private String id;
-    private int amount;
+public class WalkInCustomer extends Customer implements Billable {
+    private double offeredPrice;
 
-    public WalkInCustomer(String email, String phoneNumber, String lastName, String firstName, String id, int amount) {
-        super(email, phoneNumber, lastName, firstName);
-        this.id = id;
-        this.amount = amount;
+    public WalkInCustomer(String firstName, String lastName, String email, String phoneNumber, String customerId, double offeredPrice) {
+        super(firstName, lastName, email, phoneNumber, customerId);
+        updateOfferedPrice(offeredPrice);
+    }
+
+    public double offerAmount() {
+        return offeredPrice;
+    }
+
+    public void updateOfferedPrice(double offeredPrice) {
+        if (offeredPrice > 0)
+            this.offeredPrice = offeredPrice;
+        else throw new IllegalArgumentException();
+    }
+
+    @Override
+    public String customerType() {
+        return "Walk-In";
+    }
+
+    @Override
+    public String role() {
+        return "Walk-In Customer";
+    }
+
+    @Override
+    public String generateInvoice() {
+        return String.format("Invoice for %s | Offer: R %f", fullName(), offerAmount());
     }
 }
