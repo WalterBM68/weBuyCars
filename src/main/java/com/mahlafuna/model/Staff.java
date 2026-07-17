e00cc066b1acd521720390d34b96df857692450c
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-07-17 23:24:17 +0200

    fix(Staff.java): Staff class fixed

--- src/main/java/com/mahlafuna/model/Staff.java
+++ src/main/java/com/mahlafuna/model/Staff.java
@@ -1,18 +1,32 @@
 package com.mahlafuna.model;
 
-public abstract class Staff {
+public abstract class Staff extends Person {
     private final String employeeId;
     private int yearsOfService;
 
-    public Staff(String employeeId, int yearsOfService) {
+    public Staff(String firstName, String lastName, String email, String phoneNumber, String employeeId, int yearsOfService) {
+        super(firstName, lastName, email, phoneNumber);
         this.employeeId = employeeId;
         updateYearsOfService(yearsOfService);
     }
 
     public void updateYearsOfService(int yearsOfService) {
-        if (yearsOfService > 0)
-            this.yearsOfService = yearsOfService;
-        else throw new IllegalArgumentException();
+        if (yearsOfService < 0)
+            throw new IllegalArgumentException();
+        else this.yearsOfService = yearsOfService;
+    }
+
+    public String employeeId() {
+        return employeeId;
+    }
+
+    public int yearsOfService() {
+        return yearsOfService;
+    }
+
+    @Override
+    public String role() {
+        return "Staff";
     }
 
     public abstract double monthlySalary();
