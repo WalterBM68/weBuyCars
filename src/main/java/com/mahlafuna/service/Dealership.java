7c48b2cbf1681197240b455314ff21a07d3aafb5
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-08-04 11:26:34 +0200

    feat(Dealership.java): Dealership class few getters and setters have been implemented

--- src/main/java/com/mahlafuna/service/Dealership.java
+++ src/main/java/com/mahlafuna/service/Dealership.java
@@ -19,18 +19,19 @@ public class Dealership {
         this.staffList = new ArrayList<>();
     }
 
+    public void addVehicle(Vehicle vehicle) { this.inventory.add(vehicle); }
+
+    public void addCustomer(Customer customer) { this.customers.add(customer); }
+
+    public void addStaff(Staff staff) { this.staffList.add(staff); }
+
     public String dealershipName() {
         return dealershipName;
     }
-
     public List<Vehicle> inventory() {
         return Collections.unmodifiableList(inventory);
     }
-
-    public List<Customer> customers() {
-        return Collections.unmodifiableList(customers);
-    }
-
+    public List<Customer> customers() { return Collections.unmodifiableList(customers); }
     public List<Staff> staffList() {
         return Collections.unmodifiableList(staffList);
     }
