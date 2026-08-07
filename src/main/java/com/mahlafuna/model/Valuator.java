0dfbb33b31f282941209e293de2a03cf11cdf3a3
Bongumusa <wamanjajhb024@student.wethinkcode.co.za>
2026-08-07 11:41:29 +0200

    fix(Valuator.java): the field is fixed

--- src/main/java/com/mahlafuna/model/Valuator.java
+++ src/main/java/com/mahlafuna/model/Valuator.java
@@ -1,7 +1,7 @@
 package com.mahlafuna.model;
 
 public class Valuator extends Staff {
-    private String certification;
+    private final String certification;
 
     public static final double BASE_SALARY = 20_000.00;
     public static final double BONUS_PER_YEAR = 500.00;
